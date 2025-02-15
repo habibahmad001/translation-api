389abdb944c1fdc15ad66c388a9108a3b15bb7c6
Habib Ahmed <habibahmed001@gmail.com>
2025-02-15 07:16:18 +0500

    readme

--- database/factories/TranslationFactory.php
+++ database/factories/TranslationFactory.php
@@ -11,11 +11,20 @@ class TranslationFactory extends Factory
 
     public function definition()
     {
+        // List of valid locales (language codes)
+        $locales = ['en', 'fr', 'es', 'de', 'it', 'pt', 'ru', 'zh', 'ja', 'ar'];
+
+        // List of meaningful tags for context
+        $tags = ['mobile', 'desktop', 'web', 'admin', 'user', 'email', 'notification', 'error', 'success'];
+
+        // Generate a unique key using a combination of words and a random number
+        $key = $this->faker->word . '-' . $this->faker->word . '-' . $this->faker->randomNumber(3);
+
         return [
-            'key' => $this->faker->word,
+            'key' => $key, // Custom unique key
             'content' => $this->faker->sentence,
-            'locale' => $this->faker->locale,
-            'tag' => $this->faker->word,
+            'locale' => $this->faker->randomElement($locales), // Randomly pick a locale
+            'tag' => $this->faker->randomElement($tags), // Randomly pick a tag
         ];
     }
 }
